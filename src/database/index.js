5d88293e73fa2468e360a9828e89eba6887e0c43
Luke0195 <lucas201273@gmail.com>
2020-09-23 08:53:37 -0300

    configuraçao com a base de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,2 +1,9 @@
 // Arquivo de conexão com a base de dados
 // Será responsavel por fazer a conexão dos models com o banco
+
+const Sequelize = require('sequelize');
+const dbConfig = require('../config/database');
+const connection = new Sequelize(dbConfig);
+
+
+module.exports = connection;
